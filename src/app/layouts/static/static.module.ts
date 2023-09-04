b52b765d16a662c778c2b18c09e51d61c6b4f8bc
Pritom Kumar <pritomkucse@gmail.com>
2023-09-04 08:17:43 +0600

    Angular Layout Example

--- src/app/layouts/static/static.module.ts
+++ src/app/layouts/static/static.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import {StaticLayoutComponent} from "./static.component";
+import {LoginComponent} from "../../modules/login/login.component";
+import {FormsModule} from "@angular/forms";
+import {BrowserModule} from "@angular/platform-browser";
+import {RouterModule} from "@angular/router";
+import {SharedModule} from "../../shared/shared.module";
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+    declarations: [
+        StaticLayoutComponent,
+        LoginComponent, //register login component will use this layout
+    ],
+    imports: [
+        FormsModule,
+        CommonModule,
+        BrowserModule,
+        RouterModule,
+        SharedModule,//we will access header and footer via shared module
+    ]
 })
 export class StaticLayoutModule { }
