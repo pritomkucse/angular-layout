a6efae065faf340f74ea6002709d4dc8c43ea6d6
Pritom Kumar <pritomkucse@gmail.com>
2023-09-10 22:54:02 +0600

    Angular Lazy Loading Example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {DefaultLayoutModule} from "./layouts/default/default.module";
-import {StaticLayoutModule} from "./layouts/static/static.module";
-import { UsersComponent } from './modules/users/users.component';
+import { DefaultLayoutModule } from "./layouts/default/default.module";
+import { StaticLayoutModule } from "./layouts/static/static.module";
 
 @NgModule({
     declarations: [
         AppComponent,
-        UsersComponent,
     ],
     imports: [
         BrowserModule,
